990591ee2a322d760f0b0f895ab454686921566d
Starrbr1ght <alaynabentley3@gmail.com>
2025-12-01 08:29:11 -0600

    Add nested lists and list comprehension examples
    Added grocery lists and examples of nested lists.

--- week 13  csp/4 — Nested Lists and List Comprehensions.py
+++ week 13  csp/4 — Nested Lists and List Comprehensions.py
@@ -1,8 +1,27 @@
 # Objective:
 # Students will manipulate nested lists and understand basic list comprehensions.
+fruit =    ['apple', 'orange', 'banana', 'coconut']
+vegetables=['celery', 'carrots', 'potatos']
+meats=     ['chicken', 'fish', 'turkey']
+groceries= [fruit, vegetables, meats]
+# print(groceries[0][0]) #The first [] is either fruit, vege, or meats. (0,1,2)
+# #second [] is which item from the list you want printed out
+# print(groceries[2][2])
+# print(groceries[1][2])
+
+for collection in groceries
+    for food in collection:
+         print(food, end=' ')
+         print()
+
+
+
 
 # Key Notes:
-
+num_pad = ((1, 2, 3,),
+           (4, 5, 6,)
+           (7, 8, 9,),
+           ('*', 0, '#'))
 # A list can contain other lists.
 
 # List comprehensions provide a concise way to create lists.
